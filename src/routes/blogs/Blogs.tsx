078cae46fe679caee29962513f02aafa09fe9b7c
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-10-09 16:46:43 +0500

    NEOM-58 : Implement routing and adding placeholder pages for routing

---
+++ src/routes/blogs/Blogs.tsx
@@ -0,0 +1,6 @@
+import "./Blogs.scss";
+
+const Blogs = () => {
+  return <h1>This is Blogs Page</h1>;
+};
+export default Blogs;
---
+++ src/routes/contact-us/ContactUs.tsx
@@ -0,0 +1,6 @@
+import "./ContactUs.scss";
+
+const ContactUs = () => {
+  return <h1>This is Contact Us Page</h1>;
+};
+export default ContactUs;
